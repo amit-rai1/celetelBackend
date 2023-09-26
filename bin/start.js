85d142e3f122955feba432eb1d05ad32dc3bb086
amit-rai1 <raiamit9264@gmail.com>
2023-09-27 02:50:41 +0530

    port

--- bin/start.js
+++ bin/start.js
@@ -1,21 +1,48 @@
-'use Strict';  
+// 'use Strict';  
  
+// require('@babel/register')
+// require('@babel/polyfill');
+// const http = require ('http');
+
+// // const app = require('../app').default;
+// const app = require('../src')
+// const server = http.createServer(app);
+
+// const config = require('../config')
+// console.log(process.env.Node_env);
+// const configvalue = config.get(process.env.Node_env);
+
+// console.log(configvalue,"configvalue");
+
+// const port = configvalue.PORT;
+
+// console.log(port,"port")
+
+// server.listen(process.env.port);
+
+// server.on('listening', ()=> {
+//    console.log(`Listening on ${process.env.port}`);
+// });
+'use Strict';
+
 require('@babel/register')
 require('@babel/polyfill');
-const http = require ('http');
+const http = require('http');
 
-// const app = require('../app').default;
-const app = require('../src')
+const app = require('../src'); // Assuming this is the path to your app
 const server = http.createServer(app);
 
-const config = require('../config')
-console.log(process.env.Node_env);
+const config = require('../config');
 const configvalue = config.get(process.env.Node_env);
 
-const port = configvalue.PORTNO;
+console.log(configvalue, "configvalue");
 
-server.listen(process.env.PORT);
+const port = configvalue.PORT || 3000; // Defaulting to 3000 if PORT is not set in config
 
-server.on('listening', ()=> {
-   console.log(`Listening on ${process.env.PORT}`);
+console.log(port, "port");
+
+server.listen(port);
+
+server.on('listening', () => {
+  console.log(`Listening on ${port}`);
 });
